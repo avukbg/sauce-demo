894f6d7da9783498aa913cdea9ef3df0388530c2
avukb <avuk.bg@gmail.com>
2024-06-15 17:46:52 +0200

    Intitial Commit

--- src/test/java/Pages/Help.java
+++ src/test/java/Pages/Help.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class HelperPage extends BaseTest {
+public class Help extends BaseTest {
 
-    public HelperPage() {
+    public Help() {
         PageFactory.initElements(driver, this);
     }
 
